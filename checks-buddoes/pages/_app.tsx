6cd8b10098a3c6d4e7d455698759c74425ad79f5
ForBee.dev <code@forbee.dev>
2023-02-06 11:23:30 +0000

    start

---
+++ checks-buddoes/pages/_app.tsx
@@ -0,0 +1,33 @@
+import type { AppProps } from "next/app";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import "../styles/globals.css";
+import Head from "next/head";
+import ThirdwebGuideFooter from "../components/GitHubLink";
+
+// This is the chainId your dApp will work on.
+const activeChainId = ChainId.Mainnet;
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThirdwebProvider desiredChainId={activeChainId}>
+      <Head>
+        <title>Checks Buddies</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />
+        <meta
+          name="description"
+          content="This may or may not be a PFP Collection inspired by the great work of Jack Butcher on Checks - VV Edition"
+        />
+        <meta
+          name="keywords"
+          content="NFT"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
