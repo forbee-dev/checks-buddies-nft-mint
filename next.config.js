7ea76acd8660782688e743a32b57119d521ef294
ForBee.dev <code@forbee.dev>
2023-02-06 16:55:36 +0000

    modified:   next.config.js 	modified:   package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  useFileSystemPublicRoutes: false,
+  nextConfig,
+};
